490ef692f77992873022f13b3e1eb57556e97336
Jacob Dein <jake@jacobdein.com>
2016-09-13 16:41:54 -0400

    Adds the 'SoundExposureLevel' model to the 'metrics' app

--- server/metrics/admin.py
+++ server/metrics/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
-from .models import SoundscapeSpec
+from .models import SoundscapeSpec, SoundExposureLevel, Analysis
 admin.site.register(SoundscapeSpec)
+admin.site.register(SoundExposureLevel)
+admin.site.register(Analysis)
--- server/metrics/models.py
+++ server/metrics/models.py
@@ -1,20 +1,36 @@
 from django.db import models
 
 
-class SoundscapeSpec(models.Model):
-	id = models.IntegerField(primary_key = True)
+class SoundscapeSpec(models.Model, ):
+	id = models.AutoField(primary_key = True)
 	frequency_power = models.CharField(max_length = 500)
 	created = models.DateTimeField(auto_now_add = True)
 	modified = models.DateTimeField(auto_now = True)
 	sound = models.ForeignKey('database.Sound', on_delete = models.CASCADE, db_column = 'sound')
-	call = models.ForeignKey('Call', on_delete = models.SET_NULL, null = True, db_column = 'call')
+	analysis = models.ForeignKey('Analysis', on_delete = models.SET_NULL, null = True, db_column = 'analysis')
 	
 	def __str__(self):
 		return "{0} - {1}".format(self.id, self.sound.name)
 
 
-class Call(models.Model):
-	id = models.IntegerField(primary_key = True)
+class SoundExposureLevel(models.Model):
+	id = models.AutoField(primary_key = True)
+	sel = models.CharField(max_length = 500)
+	created = models.DateTimeField(auto_now_add = True)
+	modified = models.DateTimeField(auto_now = True)
+	sound = models.ForeignKey('database.Sound', on_delete = models.CASCADE, db_column = 'sound')
+	analysis = models.ForeignKey('Analysis', on_delete = models.SET_NULL, null = True, db_column = 'analysis')
+	
+	def __str__(self):
+		return "{0} - {1}".format(self.id, self.sound.name)
+
+
+class Analysis(models.Model):
+	
+	class Meta:
+		verbose_name_plural = 'analyses'
+	
+	id = models.AutoField(primary_key = True)
 	name = models.CharField(max_length = 50)
 	language = models.CharField(max_length = 50)
 	call = models.CharField(max_length = 500)
