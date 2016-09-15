e71b371597dae72e95fdc65fc7daf38078cbe687
Jacob Dein <jake@jacobdein.com>
2016-09-15 16:16:10 -0400

    Adds the 'AcousticComplexityIndex' model to the 'metrics' app

--- server/metrics/models.py
+++ server/metrics/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 
+class AcousticComplexityIndex(models.Model):
+	id = models.AutoField(primary_key = True)
+	aci_left = models.FloatField()
+	aci_right = models.FloatField()
+	created = models.DateTimeField(auto_now_add = True)
+	modified = models.DateTimeField(auto_now = True)
+	sound = models.ForeignKey('database.Sound', on_delete = models.CASCADE, db_column = 'sound')
+	analysis = models.ForeignKey('Analysis', on_delete = models.SET_NULL, null = True, db_column = 'analysis')
+	
+	def __str__(self):
+		return "{0} - {1}".format(self.id, self.sound.name)
+	
+	class Meta():
+		verbose_name_plural = 'acoustic complexity indices'
+
+
 class SoundscapeSpec(models.Model):
 	id = models.AutoField(primary_key = True)
 	frequency_power = models.CharField(max_length = 500)
