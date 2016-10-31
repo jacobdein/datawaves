6e4ceb0f5d696083c3b1773127a5fb14eb7138c7
Jacob Dein <jake@jacobdein.com>
2016-10-31 16:32:50 -0400

    Adds the 'SampleLocation' model to the 'geo' app

--- server/geo/models.py
+++ server/geo/models.py
@@ -10,3 +10,13 @@ class Boundary(models.Model):
 		
 	class Meta():
 		verbose_name_plural = 'Boundaries'
+
+
+class SampleLocation(models.Model):
+	probability = models.FloatField(blank = True)
+	landcover = models.IntegerField(blank = True)
+	naturalness = models.FloatField(blank = True)
+	shape = models.PointField(srid = 31254)
+	
+	def __str__(self):
+		return "{0} - land: {1}".format(self.id, self.landcover)
